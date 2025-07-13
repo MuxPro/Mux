479afa2f51ed287f19b913ce7098b06927b9b7da
NewTCP <newtcp1@hotmail.com>
2025-07-13 11:50:57 +0800

    feat: Go implementation version 1

--- mux.go
+++ mux.go
@@ -1,3 +1,16 @@
 package mux
 
 //go:generate go run github.com/v2fly/v2ray-core/v5/common/errors/errorgen
+
+import "github.com/v2fly/v2ray-core/v5/common/net"
+
+// Mux.Pro Address and Port (Updated from Mux.Cool)
+var (
+	// muxProAddress represents the target address for Mux.Pro traffic.
+	muxProAddress = net.ParseAddress("mux.pro")
+	// muxProPort represents the target port for Mux.Pro traffic.
+	muxProPort = net.Port(9528)
+)
+
+// MuxProVersion represents the current Mux.Pro protocol version.
+const MuxProVersion uint32 = 0x00000000 // MuxPro/0.0
